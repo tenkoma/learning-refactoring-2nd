45ca357f9ed6f60cf659b870a3227a7584ecf3e7
Koji Tanaka <tenkoma@gmail.com>
2023-03-19 20:04:32 +0900

    refactor: separate statement function definition and export

--- chapter1/p2-engeki/statement.mjs
+++ chapter1/p2-engeki/statement.mjs
@@ -1,4 +1,4 @@
-export function statement(invoice, plays) {
+function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
@@ -41,3 +41,5 @@ export function statement(invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
+
+export { statement };
