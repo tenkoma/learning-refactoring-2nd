9ac35ff9fe7fa7ae9d5157fd083128d598f2c03e
Koji Tanaka <tenkoma@gmail.com>
2023-03-24 01:06:27 +0900

    refactor: fix indent

--- chapter1/p2-engeki/__tests__/statement.test.js
+++ chapter1/p2-engeki/__tests__/statement.test.js
@@ -1,54 +1,54 @@
 import {statement} from "../statement.mjs";
 
 test('BigCo: hamlet', () => {
-    const plays = {"hamlet": {"name": "Hamlet", "type": "tragedy"}}
-    const invoice = {
-        "customer": "BigCo",
-        "performances": [
-            {
-                "playID": "hamlet",
-                "audience": 55
-            }
-        ]
-    };
-    const expected = `Statement for BigCo
+  const plays = {"hamlet": {"name": "Hamlet", "type": "tragedy"}}
+  const invoice = {
+    "customer": "BigCo",
+    "performances": [
+      {
+        "playID": "hamlet",
+        "audience": 55
+      }
+    ]
+  };
+  const expected = `Statement for BigCo
   Hamlet: $650.00 (55 seats)
 Amount owed is $650.00
 You earned 25 credits
 `;
-    expect(statement(invoice, plays)).toBe(expected);
+  expect(statement(invoice, plays)).toBe(expected);
 });
 
 test('BigCo: all', () => {
-    const plays = {
-        "hamlet": {"name": "Hamlet", "type": "tragedy"},
-        "as-like": {"name": "As You Like It", "type": "comedy"},
-        "othello": {"name": "Othello", "type": "tragedy"}
-    };
-    const invoice = {
-        "customer": "BigCo",
-        "performances": [
-            {
-                "playID": "hamlet",
-                "audience": 55
-            },
-            {
-                "playID": "as-like",
-                "audience": 35
-            },
-            {
-                "playID": "othello",
-                "audience": 40
-            }
-        ]
-    };
-    const expected = `Statement for BigCo
+  const plays = {
+    "hamlet": {"name": "Hamlet", "type": "tragedy"},
+    "as-like": {"name": "As You Like It", "type": "comedy"},
+    "othello": {"name": "Othello", "type": "tragedy"}
+  };
+  const invoice = {
+    "customer": "BigCo",
+    "performances": [
+      {
+        "playID": "hamlet",
+        "audience": 55
+      },
+      {
+        "playID": "as-like",
+        "audience": 35
+      },
+      {
+        "playID": "othello",
+        "audience": 40
+      }
+    ]
+  };
+  const expected = `Statement for BigCo
   Hamlet: $650.00 (55 seats)
   As You Like It: $580.00 (35 seats)
   Othello: $500.00 (40 seats)
 Amount owed is $1,730.00
 You earned 47 credits
 `;
-    expect(statement(invoice, plays)).toBe(expected);
+  expect(statement(invoice, plays)).toBe(expected);
 });
 
--- chapter1/p2-engeki/statement.mjs
+++ chapter1/p2-engeki/statement.mjs
@@ -1,51 +1,51 @@
 function statement(invoice, plays) {
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 
-    function amountFor(aPerformance) {
-        let result = 0;
+  function amountFor(aPerformance) {
+    let result = 0;
 
-        switch (playFor(aPerformance).type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 20) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 20) {
+          result += 1000 * (aPerformance.audience - 30);
         }
-        return result;
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
-
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
-
-    const format = Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-        minimumFractionDigits: 2}).format;
-
-    for (let perf of invoice.performances) {
-        // ボリューム特典のポイントを加算
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // 喜劇のときは10人につき、さらにポイントを加算
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-        // 注文の内訳を出力
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
-    }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
     return result;
+  }
+
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+
+  const format = Intl.NumberFormat("en-US",
+    { style: "currency", currency: "USD",
+      minimumFractionDigits: 2}).format;
+
+  for (let perf of invoice.performances) {
+    // ボリューム特典のポイントを加算
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 喜劇のときは10人につき、さらにポイントを加算
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    // 注文の内訳を出力
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
 }
 
 export {statement};
