d37adc8334094a04cff66da31cb082c78cfc9391
Koji Tanaka <tenkoma@gmail.com>
2023-03-24 01:20:58 +0900

    refactor: Change Function Declaration (format() to usd())

--- chapter1/p2-engeki/statement.mjs
+++ chapter1/p2-engeki/statement.mjs
@@ -33,10 +33,10 @@ function statement(invoice, plays) {
     return result;
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
       { style: "currency", currency: "USD",
-        minimumFractionDigits: 2}).format(aNumber);
+        minimumFractionDigits: 2}).format(aNumber/100);
   }
 
   let totalAmount = 0;
@@ -47,10 +47,10 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 注文の内訳を出力
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
