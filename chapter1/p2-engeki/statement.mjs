4f695fa7f0786213c03d20c860909e9c5e916fa0
Koji Tanaka <tenkoma@gmail.com>
2023-03-26 17:22:51 +0900

    refactor: rename function name

--- chapter1/p2-engeki/statement.mjs
+++ chapter1/p2-engeki/statement.mjs
@@ -46,7 +46,7 @@ function statement(invoice, plays) {
         minimumFractionDigits: 2}).format(aNumber/100);
   }
 
-  function appleSauce() {
+  function totalAmount() {
     let totalAmount = 0;
     for (let perf of invoice.performances) {
       totalAmount += amountFor(perf);
@@ -60,7 +60,7 @@ function statement(invoice, plays) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
 
-  result += `Amount owed is ${usd(appleSauce())}\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
