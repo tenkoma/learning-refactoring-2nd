7d02e238084bd28f0d2505ebb777cdd0e416b096
Koji Tanaka <tenkoma@gmail.com>
2023-03-26 20:16:27 +0900

    feat: add htmlStatement

--- chapter1/p2-engeki/statement.mjs
+++ chapter1/p2-engeki/statement.mjs
@@ -4,6 +4,10 @@ function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
@@ -16,10 +20,25 @@ function renderPlainText(data) {
   return result;
 }
 
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n`;
+  for (let perf of data.performances) {
+    // 注文の内訳を出力
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td><td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
 function usd(aNumber) {
   return new Intl.NumberFormat("en-US",
     { style: "currency", currency: "USD",
       minimumFractionDigits: 2}).format(aNumber/100);
 }
 
-export {statement};
+export {statement, htmlStatement};
