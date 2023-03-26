7b0211aaaf9c20cb9740c49df76474470ec220e1
Koji Tanaka <tenkoma@gmail.com>
2023-03-26 21:45:32 +0900

    refactor: Replace Conditional with Polymorphism (ComedyCalculator.amount)

--- chapter1/p2-engeki/createStatementData.mjs
+++ chapter1/p2-engeki/createStatementData.mjs
@@ -42,6 +42,14 @@ class TragedyCalculator extends PerformanceCalculator {
 }
 
 class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
