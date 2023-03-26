09a8b56e0d0a48d635f8a1f6482f44e00c0f1cf4
Koji Tanaka <tenkoma@gmail.com>
2023-03-26 20:23:39 +0900

    test: add test2 for htmlStatement

--- chapter1/p2-engeki/__tests__/statement.test.js
+++ chapter1/p2-engeki/__tests__/statement.test.js
@@ -73,3 +73,39 @@ test('htmlStatement1', () => {
 `;
   expect(htmlStatement(invoice, plays)).toBe(expected);
 });
+
+test('htmlStatement2', () => {
+  const plays = {
+    "hamlet": {"name": "Hamlet", "type": "tragedy"},
+    "as-like": {"name": "As You Like It", "type": "comedy"},
+    "othello": {"name": "Othello", "type": "tragedy"}
+  };
+  const invoice = {
+    "customer": "BigCo",
+    "performances": [
+      {
+        "playID": "hamlet",
+        "audience": 55
+      },
+      {
+        "playID": "as-like",
+        "audience": 35
+      },
+      {
+        "playID": "othello",
+        "audience": 40
+      }
+    ]
+  };
+  const expected = `<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr>
+  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
+`;
+  expect(htmlStatement(invoice, plays)).toBe(expected);
+});
