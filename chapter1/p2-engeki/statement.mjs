4deb6876d227a28f285295c6fc6b85ee4278f3fa
Koji Tanaka <tenkoma@gmail.com>
2023-03-26 16:33:45 +0900

    refactor: Extract function totalVolumeCredits()

--- chapter1/p2-engeki/statement.mjs
+++ chapter1/p2-engeki/statement.mjs
@@ -32,6 +32,13 @@ function statement(invoice, plays) {
     if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
     return result;
   }
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
@@ -47,10 +54,7 @@ function statement(invoice, plays) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits();
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
