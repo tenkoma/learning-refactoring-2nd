51565585f6b0f54e03b8ae8845dec1cba622adcc
Koji Tanaka <tenkoma@gmail.com>
2023-03-26 18:31:02 +0900

    refactor: Extract Function (renderPlainText)

--- chapter1/p2-engeki/statement.mjs
+++ chapter1/p2-engeki/statement.mjs
@@ -1,4 +1,8 @@
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
